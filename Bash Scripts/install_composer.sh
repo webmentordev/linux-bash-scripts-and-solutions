a102163314d462d123f0ce4288b3157cf135a7ac
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2023-10-08 21:56:40 +0500

    Composer fix and extra php libraries

--- Bash Scripts/install_composer.sh
+++ Bash Scripts/install_composer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install wget php-cli php-zip unzip phpunit curl php-pear php-dev php-xml php-pdo php-common -y
+sudo apt-get install wget php-cli php-zip unzip phpunit php-mysql curl php-pear php-dev php-xml php-pdo php-common php-mbstring -y
 curl -sS https://getcomposer.org/installer -o /tmp/composer-setup.php
 HASH=`curl -sS https://composer.github.io/installer.sig`
 php -r "if (hash_file('SHA384', '/tmp/composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 sudo php /tmp/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-sudo apt-get install phpunit -y
