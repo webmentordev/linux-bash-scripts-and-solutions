bd81a223e1e9d8c870d33cc440081bb3f01e8d97
Muhammad Ahmer Tahir <82140155+webmentordev@users.noreply.github.com>
2024-04-22 16:07:46 +0500

    Removed & shifted unnecessary scripts

--- Bash Scripts/install_config_openssh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install openssh-server -y
-
-sudo ufw enable
-sudo ufw allow 22
-
-echo "OpenSSH Server Successfully Installed & Configured in Firewall"
--- Bash Scripts/install_steam.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt-get install lib32gcc-s1
-mkdir steam
-cd steam
-sudo apt-get install curl
-curl -sql "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zxvf -
-sudo apt-get install tmux screen -y
